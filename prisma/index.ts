2f50da3d36b1b74bb89958b1bad56091c95fbdd3
masamun <masamun@outlook.com>
2024-03-10 18:21:47 +0900

    prisma test

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,1 +1,13 @@
-export { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma as PrismaEdge, PrismaClient as PrismaClientEdge } from "@prisma/client/edge";
+
+let prisma = PrismaEdge;
+let prismaClient = PrismaClientEdge;
+if (process.env.NODE_ENV === "development") {
+  console.info("prisma module development");
+  prisma = Prisma;
+  prismaClient = PrismaClient;
+}
+
+export { prisma as Prisma };
+export { prismaClient as PrismaClient };
