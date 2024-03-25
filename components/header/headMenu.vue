cacba153ee5ffa46a300e1d6d15067688c4b5c7d
masamun <masamun@outlook.com>
2024-03-26 06:27:01 +0900

    githubへのリンクを追加

--- components/header/headMenu.vue
+++ components/header/headMenu.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="pl-4 pr-2 text-white flex justify-start items-center">
+  <div class="pl-4 pr-2 text-white flex justify-between items-center">
     <nuxt-link to="/">HOME</nuxt-link>
+    <div class="flex h-6 gap-4 mr-2 items-center">
+      <nuxt-link to="https://github.com/masamun/wocus" target="_blank" no-rel
+        ><img src="~/assets/icon/github/github-mark-white.svg" class="w-6 h-6" alt="wocus github repository"
+      /></nuxt-link>
+    </div>
   </div>
 </template>
